38a25089593b88d7efb5220eb067865b34201f8a
Kuan Butts <kuanbutts@yahoo.com>
2015-05-27 09:11:21 -0400

    updated port listen

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.get('/geojson/proposed', function (req, res) {
   });
 })
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Coaxs app listening at http://%s:%s', host, port);
