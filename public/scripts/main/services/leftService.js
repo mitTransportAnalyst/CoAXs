fb5c8caf85cf7f7f1fba1fe2a5fcd18774fab25d
Kuan Butts <kuanbutts@yahoo.com>
2015-07-22 19:00:43 -0400

    targetPOIUsers fixed

--- public/scripts/main/services/leftService.js
+++ public/scripts/main/services/leftService.js
@@ -30,57 +30,14 @@ coaxsApp.service('leftService', function (leafletData) {
     })
   };
 
-   
-  // OLD VERSION (PRE-DAY IN THE LIFE)
-  // this.targetPOIUsers = function (poiUsers, id) {
-  //   poiUsers.eachLayer( function (layer) {
-  //     if (layer.options.userId == id) {
-  //       layer.setStyle({opacity : 1, fillOpacity : 1});
-  //     } else {
-  //       layer.setStyle({opacity : 0, fillOpacity : 0});
-      
-  //     }
-  //   });
-  // }
 
-  // UPDATED VERSION FOR ACCOMODATING MULTIPLE ICON TYPES
-    this.targetPOIUsers = function (poiUserPoints, id) {
-      console.log("leftService receives clicked id" +" "+ id);
-      //console.log(JSON.stringify(poiUsers));
-      console.log("userPoints", poiUserPoints);
-      poiUserPoints.eachLayer( function (marker) {
-        console.log("marker", marker);
+  this.targetPOIUsers = function (poiUsers, id) {
+    poiUsers.eachLayer( function (layer) { console.log(layer)
+      if (layer.userId == id) { layer.setOpacity(1); } 
+      else { layer.setOpacity(0); }
+    });
+  }
 
-      if (marker.userId == id) {
-          var icon = marker.options.icon.options.iconUrl;
-         //console.log("icon URL at leftService" + " " +icon);
-          var icon_on = L.Icon.extend({
-          options : {
-            iconUrl      : icon,
-            iconSize     : [25, 25],
-            iconAnchor   : [8, 18],
-            popupAnchor  : [0, -15],
-            opacity      : 1,
-            className    : 'icon-on'
-          }
-        });
-        marker.setIcon(new icon_on());
-      } else {
-          var icon = marker.options.icon.options.iconUrl;
-          var icon_off = L.Icon.extend({
-          options : {
-            iconUrl      : icon,
-            iconSize     : [25, 25],
-            iconAnchor   : [8, 18],
-            popupAnchor  : [0, -15],
-            opacity      : .3,
-            className    : 'icon-off',
-          }
-          });   
-        marker.setIcon(new icon_off());    
-        }
-      });
-    }
 });
 
 
