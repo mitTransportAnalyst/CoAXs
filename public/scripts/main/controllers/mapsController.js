7561949a1f4890c165d18a2e6be2b59a28612cf7
Kuan Butts <kuanbutts@yahoo.com>
2015-08-03 15:56:39 -0400

    resolves issue 40, closing route info box does not deselect route

--- public/scripts/main/controllers/mapsController.js
+++ public/scripts/main/controllers/mapsController.js
@@ -389,6 +389,7 @@ coaxsApp.controller('mapsController', function ($http, $scope, $state, leafletDa
 
   // update a specific route within a corridor
   $scope.updateTargetFeature = function (variant) {
+    $scope.routeDetails = true;
     var routeId = variant ? variant.routeId : undefined
     var station = variant ? variant.station : 0;
     var routeColor = variant ? $scope.routes[variant.routeId][0].options.base.routeColor : undefined;
