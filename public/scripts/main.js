026fff892c563791caec1bf6c02c5392f5e00a40
Kuan Butts <kuanbutts@yahoo.com>
2015-09-17 12:09:59 -0400

    moved ng Route to own module

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,6 @@
 // this is the main router and allows you to create multiple views the main app (currently we only have one)
 
-var coaxsApp = angular.module('coaxsApp', ['coaxsFilters', 'ui.router', 'ui.bootstrap', 'leaflet-directive']);
+var coaxsApp = angular.module('coaxsApp', ['ngRoute', 'coaxsFilters', 'ui.router', 'ui.bootstrap', 'leaflet-directive']);
 
 coaxsApp.config(function($stateProvider, $urlRouterProvider) {
     
