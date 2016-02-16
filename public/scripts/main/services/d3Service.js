2a9d4fb50df5e9bf1bbb1028cdcd4c61d2017e9d
ansons <ansonstewart@gmail.com>
2016-02-16 12:24:44 -0500

    Vertical line on d3 chart to track vectorIso value

--- public/scripts/main/services/d3Service.js
+++ public/scripts/main/services/d3Service.js
@@ -1,11 +1,11 @@
 // this is the boiler to visualize the d3 graph
 coaxsApp.service('d3Service', function () {
 
+
   // all you need to know is that you feed in the data array of length 120 to this function and it'll render
-  this.drawGraph = function (data, compare) {  
-	  
-    var vis   = d3.select("#compPlot"),
-      WIDTH   = 300,
+  this.drawGraph = function (cutoff, data, compare) {  
+  
+       var WIDTH   = 300,
       HEIGHT  = 200,
       MARGINS = {
         top    : 20,
@@ -13,6 +13,7 @@ coaxsApp.service('d3Service', function () {
         bottom : 20,
         left   : 50
       },
+	vis   = d3.select("#compPlot"),
 
       combined = compare ? data.concat(compare) : data,
 
@@ -29,7 +30,7 @@ coaxsApp.service('d3Service', function () {
           return d.y;
         })
       ]),
-
+	  
       xAxis = d3.svg.axis()
         .scale(xRange)
 		.ticks(6)
@@ -87,9 +88,9 @@ coaxsApp.service('d3Service', function () {
     .attr("d", lineFunc(data))
     .attr("class", "line")
     .style("stroke-width", "1");
-
-
-  if (compare) { 
+	
+	
+	if (compare) { 
     vis.append("svg:path")
       .attr("d", lineFunc(compare))
       .attr("class", "lineC")
@@ -103,10 +104,10 @@ coaxsApp.service('d3Service', function () {
 		.attr("y", 40)
         .attr("x", 175)
 		.style("text-anchor","middle")
-        .html( function (){
-		  return d3.format(",")(d3.round(data[60].y-compare[60].y,-2)) + " more within 1 hr.";
+        .style("opacity", 0.85)
+		.html( function (){
+		  return d3.format(",")(d3.round(data[cutoff*5].y-compare[cutoff*5].y,-2)) + " more w/in " + 5*cutoff + " min.";
 		});  
-
   }
   else{
 	vis.append("svg:g")
@@ -116,12 +117,26 @@ coaxsApp.service('d3Service', function () {
 		.attr("y", 40)
         .attr("x", 175)
 		.style("text-anchor","middle")
+		.style("opacity", 0.85)
         .html( function (){
-		  return d3.format(",")(d3.round(data[60].y,-3))  + " within 1 hr.";
+		  return d3.format(",")(d3.round(data[cutoff*5].y,-3))  + " within " + 5*cutoff + " min.";
 		});  
 
 	}
-  }
+	
+vis.append("svg:line")
+	.attr("id", "sliderLine")
+	.attr("class", "line")
+	.attr("x1", MARGINS.left+cutoff*5*(WIDTH-MARGINS.left-MARGINS.right)/120)
+	//.attr("y1", MARGINS.top+10)
+	.attr("y1", yRange(data[cutoff*5].y))
+	.attr("x2", MARGINS.left+cutoff*5*(WIDTH-MARGINS.left-MARGINS.right)/120)
+	.attr("y2", HEIGHT-MARGINS.bottom)
+	.style("stroke", "rgb(255,255,255)")
+	.style("opacity", 0.85);
+
+
+    }
 
 });
 
